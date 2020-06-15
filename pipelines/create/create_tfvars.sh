8c849c3d9829a39f6e097000f3de4cb1410bb33d
Miguel Liebana <miguel.liebana@sony.com>
2020-06-15 10:57:25 +0000

    Rename ENVIRONMENT to ENV_TYPE in create_tfvars.sh

--- pipelines/create/create_tfvars.sh
+++ pipelines/create/create_tfvars.sh
@@ -6,6 +6,6 @@
 
 echo "# File automatically generated from Jenkins on `date`"
 echo "environment_name = \"${ENV_NAME}\""
-echo "environment_tag = \"${ENVIRONMENT}\""
+echo "environment_tag = \"${ENV_TYPE}\""
 echo "owner = \"${OWNER}\""
 echo "team = \"${TEAM}\""
